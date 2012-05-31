b2dee509cd02a926e0a28d49a48ce91db2f56c3b
Ilya Baryshev <baryshev@gmail.com>
2012-05-31 14:01:28 +0400

    Вьюха для главной #6

--- meetup/urls.py
+++ meetup/urls.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 from django.views.generic.base import TemplateView
+from views import IndexPage
 
 urlpatterns = patterns('',
-    url('^$', TemplateView.as_view(template_name='index.html'))
+    url('^$', IndexPage.as_view(), name='index'),
+    url('^event/(?P<pk>\d+)/$', TemplateView.as_view(template_name='index.html'), name='event'),
+    url('^talk/(?P<pk>\d+)/$', TemplateView.as_view(template_name='index.html'), name='talk'),
 )
--- meetup/views.py
+++ meetup/views.py
@@ -1,3 +1,15 @@
-# Create your views here.
+from django.views.generic.list import ListView
+from models import Event
 
 
+class IndexPage(ListView):
+    template_name = 'index.html'
+    context_object_name = 'events'
+    model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexPage, self).get_context_data(**kwargs)
+        context.update({
+            'next_event': Event.objects.latest()
+        })
+        return context
