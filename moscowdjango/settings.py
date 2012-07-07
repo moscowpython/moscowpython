40ea77260e8d281b7f1628b78fd54b0dcee164b7
Ilya Baryshev <baryshev@gmail.com>
2012-07-07 18:53:19 +0400

    Real info of next meetup in right sidebar + photos in fixtures

--- moscowdjango/settings.py
+++ moscowdjango/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Moscow'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -37,7 +37,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
