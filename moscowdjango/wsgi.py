e5c6c657179b37066810274d22fb5ab87e7562da
Ilya Baryshev <baryshev@gmail.com>
2012-07-09 17:13:15 +0400

    We're online!

--- moscowdjango/wsgi.py
+++ moscowdjango/wsgi.py
@@ -6,6 +6,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangodocs.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# http auth during private alpha
-logins = [('django', 'uncha!ned')]
-application = BasicAuth(application, users=logins)
+# hurray, we're on air!
+#logins = [('django', 'uncha!ned')]
+#application = BasicAuth(application, users=logins)
