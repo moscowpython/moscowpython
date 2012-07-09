0784d9795921b558ee545a9f779a888468502a38
Ilya Baryshev <baryshev@gmail.com>
2012-07-09 11:41:17 +0400

    Facebook group in menu

--- meetup/context.py
+++ meetup/context.py
@@ -7,13 +7,14 @@ def menu(request):
     menu_items = (
         {'title':u'Главная', 'url':'index'},
         {'title':u'События', 'url':'events'},
+        {'title':u'Группа на Фейсбуке', 'url':'http://www.facebook.com/groups/MoscowDjango/'},
         {'title':u'О проекте', 'url':None},
-        {'title':u'Полезные ссылки', 'url':None}
     )
 
     for item in menu_items:
         if item['url'] is not None:
-            item['url'] = reverse_lazy(item['url'])
+            if not item['url'].startswith('http'):
+                item['url'] = reverse_lazy(item['url'])
 
     return {'menu_items': menu_items}
 
