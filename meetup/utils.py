5fade33b91615caa78ae5ae7977e99b7a18dec40
Ilya Baryshev <baryshev@gmail.com>
2012-10-27 20:15:28 +0400

    Improved interaction with TimePad API #49

--- meetup/utils.py
+++ meetup/utils.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
+import logging
+import os
 import requests
+logger = logging.getLogger('subscribe')
 
 
 def subscribe_mail(email):
-    response = requests.post('http://moscowdjango.timepad.ru/org/subscribe', data={'email': email})
-    if response.status_code == 200 and u'Вы подписались' in response.text:
-        return True
-    else:
+    """ Subscribe user via Timepad
+
+        Docs: https://github.com/timepad/timepad_api/wiki/%D0%94%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B0%D0%B4%D1%80%D0%B5%D1%81%D0%BE%D0%B2-%D0%B2-%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D1%80%D0%B0%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B8
+    """
+    try:
+        payload = {
+            'id': os.environ['TIMEPAD_API_KEY'],
+            'code': os.environ['TIMEPAD_ORG_ID'],
+            'm_id': os.environ['TIMEPAD_MAILLIST_ID'],
+            'i0_email': email
+        }
+    except KeyError as e:
+        logger.error('Can not subscribe, missing key %s' % e)
+        return False
+
+    try:
+        response = requests.get('http://timepad.ru/api/maillist_add_items/', params=payload)
+    except requests.RequestException:
+        logger.error('Timepad is unavailable')
+        return False
+
+    try:
+        return response.status_code == 200 and response.json['result'] == 'ok'
+    except KeyError:
         return False
--- tests/test_subscribe.py
+++ tests/test_subscribe.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
-from unittest.case import TestCase
-
-from mock import Mock, patch, MagicMock
+from unittest import TestCase
+from mock import patch, MagicMock
 from meetup.views import ajax_subscribe
 
 
+@patch.dict('os.environ', TIMEPAD_API_KEY='xxx', TIMEPAD_ORG_ID='123', TIMEPAD_MAILLIST_ID='1')
 class SubscribeTest(TestCase):
 
     def setUp(self):
-        self.request = MagicMock()
+        self.request = MagicMock(name='email subscribe')
         self.request.POST.__getitem__.return_value = 'foo@bar.buz'
 
-    @patch('meetup.utils.requests.post')
-    def test_email_ok(self, response_mock):
-        response_mock.return_value = MagicMock(status_code=200, text=u'Поздравляем, Вы подписались')
+    @patch('meetup.utils.requests.get')
+    def test_email_ok(self, requests_mock):
+        requests_mock.return_value = MagicMock(status_code=200, json={
+            'itemsDuplicate': 0,
+            'itemsWrongFormat': 0,
+            'result': "ok",
+            'itemsAdded': 1
+        })
         response = ajax_subscribe(self.request)
         self.assertEquals(response.content, 'OK')
 
-    @patch('meetup.utils.requests.post')
-    def test_email_fail(self, response_mock):
-        response_mock.return_value = MagicMock(status_code=200, text=u'Все плохо')
+    @patch('meetup.utils.requests.get')
+    def test_email_fail(self, requests_mock):
+        requests_mock.return_value = MagicMock(status_code=200, json={
+            'error': "wrong_code"
+        })
         response = ajax_subscribe(self.request)
         self.assertEquals(response.content, 'Failed')
