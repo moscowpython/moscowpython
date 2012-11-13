ca686f625b9a1b1bcbdf12b712a1b247ae59f4e7
Ilya Baryshev <baryshev@gmail.com>
2012-11-13 10:31:34 +0400

    Clear description for on air switch #55

---
+++ meetup/forms.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.forms import ModelForm
+from models import Event
+
+
+class EventAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(EventAdminForm, self).__init__(*args, **kwargs)
+        self.fields['manual_on_air'].widget.choices = \
+            ((u'1', 'Автоматически'),
+             (u'2', 'Вкл'),
+             (u'3', 'Выкл'))
+
+    class Meta:
+        model = Event
