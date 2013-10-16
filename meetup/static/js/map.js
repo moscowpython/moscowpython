a9b3770fa2389af3563dcc439ecc746939e64336
piumosso <golikoffartem@gmail.com>
2013-10-16 16:45:29 +0400

    map.js move to the end of the page

--- meetup/static/js/map.js
+++ meetup/static/js/map.js
@@ -1,11 +1,15 @@
 $(function(){
+    if (!window['eventMapParams']) {
+        return;
+    }
+
     ymaps.ready(function(){
         var myMap = new ymaps.Map("map", {
-            center: event.coords,
+            center: eventMapParams.coords,
             zoom: 15
         }),
-        myPlacemark = new ymaps.Placemark(event.coords, {
-            balloonContent: event.baloonContent
+        myPlacemark = new ymaps.Placemark(eventMapParams.coords, {
+            balloonContent: eventMapParams.baloonContent
         });
         myMap.geoObjects.add(myPlacemark);
         myMap.controls.add('smallZoomControl');
