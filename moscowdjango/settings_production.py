174a24c06e3c5c634331a3944d126a9b688dfc10
Ilya Baryshev <baryshev@gmail.com>
2015-02-18 09:16:50 +0300

    Sync production and staging settings

--- moscowdjango/settings_production.py
+++ moscowdjango/settings_production.py
@@ -18,15 +18,8 @@ AWS_S3_SECURE_URLS = True
 DEFAULT_FILE_STORAGE = 'moscowdjango.amazon.DefaultStorage'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DEFAULT_S3_PATH = "media"
-STATIC_S3_PATH = "static"
 MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
 MEDIA_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-STATIC_ROOT = "/%s/" % STATIC_S3_PATH
-STATIC_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
 
 # Django compressor
 COMPRESS_ENABLED = False
-COMPRESS_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
-COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-COMPRESS_ROOT = '/static/'
-COMPRESS_OUTPUT_DIR = 'CACHE'
