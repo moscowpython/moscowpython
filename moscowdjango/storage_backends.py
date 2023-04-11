7fceaafd3ff81cb1cd7f4325676fad2ba97cb2ae
Mikhael Korneev <korneevm@gmail.com>
2023-04-12 00:42:00 +0700

    Change to S3StaticStorage backend

--- moscowdjango/storage_backends.py
+++ moscowdjango/storage_backends.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from django.conf import settings
 
-from storages.backends.s3boto3 import S3ManifestStaticStorage
+from storages.backends.s3boto3 import S3StaticStorage
 
 
-class S3StaticStorage(S3ManifestStaticStorage):
+class S3StaticStorage(S3StaticStorage):
     access_key = settings.STATIC_AWS_ACCESS_KEY_ID
     secret_key = settings.STATIC_AWS_SECRET_ACCESS_KEY
     bucket_name = settings.STATIC_AWS_STORAGE_BUCKET_NAME
