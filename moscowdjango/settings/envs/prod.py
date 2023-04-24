3aedcb4c802e2b5140546a230f091ddd0c487c2b
Mikhael Korneev <korneevm@gmail.com>
2023-04-24 15:05:54 +0700

    Add 45.8.250.219 to allowed hosts

--- moscowdjango/settings/envs/prod.py
+++ moscowdjango/settings/envs/prod.py
@@ -12,4 +12,4 @@ class Prod(Staging):
 
     CSRF_TRUSTED_ORIGINS = values.ListValue(['https://moscowpython.ru', 'https://www.moscowpython.ru'])
 
-    ALLOWED_HOSTS = values.ListValue(['moscowpython.ru', 'www.moscowpython.ru', 'msk.python.ru'])
+    ALLOWED_HOSTS = values.ListValue(['moscowpython.ru', 'www.moscowpython.ru', 'msk.python.ru', '45.8.250.219'])
